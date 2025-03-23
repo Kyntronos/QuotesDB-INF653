678e3fce52993fac94ad0a69f19db350c72f63eb
Kyle Cassity <166655209+Kyntronos@users.noreply.github.com>
2025-03-23 16:59:42 -0500

    reuploading api
    reuploading api with larger changes

---
+++ api/authors/read_single.php
@@ -0,0 +1,34 @@
+<?php
+    header('Access-Control-Allow-Origin: *');
+    header('Content-Type: application/json');
+
+    include_once('../../config/Database.php');
+    include_once('../../models/Authors.php');
+
+    $database = new Database();
+    $db = $database->connect();
+
+    //Instantiate quote
+
+    $Author = new Authors($db);
+
+    //Get ID
+
+    $Author->id = isset($_GET['id']) ? $_GET['id'] : null;
+
+    if($Quote-> id === null){
+        echo json_encode(array('message' => 'No quote ID provided.'));
+        exit;
+    }
+
+    //Get post
+    $Author->read_single();
+
+    //Create array
+    $auth_arr = array(
+        'id' => $Author->id,
+        'author' => $Author->author,
+    );
+
+    //Make JSON
+    print_r(json_encode($auth_arr));
