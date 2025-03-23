1b91578fe5a6ad54d56e8128a27b7a5bbaaf461c
Kyle Cassity <166655209+Kyntronos@users.noreply.github.com>
2025-03-23 16:28:44 -0500

    reuploading
    reuploading API after changes

---
+++ api/quotes/create.php
@@ -0,0 +1,33 @@
+<?php
+    header('Access-Control-Allow-Origin: *');
+    header('Content-Type: application/json');
+    header('Access-Control-Allow-Methods: POST');
+    header('Access-Control-Allow-Methods: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Authorization, X-Requested-With');
+
+    include_once('../../config/Database.php');
+    include_once('../../models/Quote.php');
+
+    $database = new Database();
+    $db = $database->connect();
+
+    //Instantiate quote
+
+    $Quote = new Quote($db);
+
+    //Get raw data
+    $data = json_decode(file_get_contents('php://input'));
+
+    $Quote->quote = $data->quote;
+    $Quote->author_id = $data->author_id;
+    $Quote->category_id = $data->category_id;
+
+    //Create post
+    if($Quote->create()){
+        echo json_encode(
+            array('message' => 'Post Created')
+        );
+    } else {
+        echo json_encode(
+            array('message' => "Post Not Created")
+        );
+    }
---
+++ api/quotes/read.php
@@ -0,0 +1,47 @@
+<?php
+    header('Access-Control-Allow-Origin: *');
+    header('Content-Type: application/json');
+
+    include_once('../../config/Database.php');
+    include_once('../../models/Quote.php');
+
+    $database = new Database();
+    $db = $database->connect();
+
+    //Instantiate quote
+
+    $Quote = new Quote($db);
+
+    //quote query
+    $result = $Quote->read();
+
+    //get row count
+    $num = $result->rowCount();
+
+    //Check if any quotes
+    if($num > 0){
+        //quotes array
+        $quotes_arr = array();
+        //$quotes_arr['data'] = array();
+
+        while($row = $result->fetch(PDO::FETCH_ASSOC)){
+            extract($row);
+
+            $quote_item = array(
+                'id' => $id,
+                'quote' => $quote,
+                'author_id' => $author_id,
+                'category_id' => $category_id
+                
+            );
+            //push to 'data'
+            array_push($quotes_arr, $quote_item);
+        }
+
+        //turn to JSON
+        echo json_encode($quotes_arr);
+    } else {
+        //No quotes
+        echo json_encode(array('message' => 'No quotes found'));
+
+    }
