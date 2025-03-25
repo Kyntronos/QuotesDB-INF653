e19c61248455390a63fe334ff6a928844a896ef9
Kyle Cassity <166655209+Kyntronos@users.noreply.github.com>
2025-03-25 17:42:14 -0500

    reup api and root index
    reup api with changes and uploading root index

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+    // Disable directory listing by sending a 403 Forbidden status code
+if (basename($_SERVER['PHP_SELF']) !== 'index.php') {
+    http_response_code(403);
+    exit("Access Forbidden");
+}
+
+// Show your message
+echo "Mid-term Proj: REST API";
