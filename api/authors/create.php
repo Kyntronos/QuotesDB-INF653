70cf10c3c56718749aef55fee0f462267dab64fa
Kyle Cassity <166655209+Kyntronos@users.noreply.github.com>
2025-03-25 15:38:49 -0500

    reuploading api
    reuploading api with organizational changes to update and create

---
+++ api/authors/create.php
@@ -0,0 +1,42 @@
+<?php
+    header('Access-Control-Allow-Origin: *');
+    header('Content-Type: application/json');
+    header('Access-Control-Allow-Methods: POST');
+    header('Access-Control-Allow-Methods: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Authorization, X-Requested-With');
+
+    include_once('../../config/Database.php');
+    include_once('../../models/Authors.php');
+
+    $database = new Database();
+    $db = $database->connect();
+
+    //Instantiate author
+
+    $Authors = new Authors($db);
+
+    //Get raw data
+    $data = json_decode(file_get_contents('php://input'));
+
+    
+
+    //$Authors->id = $data->id;
+    $Authors->author = $data->author;
+
+    if (!isset($data->id, $data->author)) {
+        echo json_encode(['message' => 'Missing Required Parameters']);
+        exit;
+    }
+
+    //Create post
+    if($Authors->create()){
+        echo json_encode(
+            array(
+                'id' => $Authors->id,
+                'author'=> $Authors->author
+            )
+        );
+    } else {
+        echo json_encode(
+            array('message' => "Author Not Created")
+        );
+    }
