9e0d5c848555ab5eae8e1bbdb8ad9b6b48084979
Kyle Cassity <166655209+Kyntronos@users.noreply.github.com>
2025-03-25 16:10:51 -0500

    reuploading api
    reuploading api after a change to quotes update and create

---
+++ api/quotes/update.php
@@ -0,0 +1,67 @@
+<?php
+    header('Access-Control-Allow-Origin: *');
+    header('Content-Type: application/json');
+    header('Access-Control-Allow-Methods: PUT');
+    header('Access-Control-Allow-Methods: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Authorization, X-Requested-With');
+
+    include_once('../../config/Database.php');
+    include_once('../../models/Quote.php');
+
+    $database = new Database();
+    $db = $database->connect();
+
+    //Instantiate quote
+
+    $Quote = new Quote($db);
+
+    //Get raw data
+    $data = json_decode(file_get_contents('php://input'));
+
+    $requiredFields = ['id', 'quote', 'author_id', 'category_id'];
+    $missingFields = [];
+    foreach ($requiredFields as $field) {
+        if (!property_exists($data, $field)) {
+            $missingFields[] = $field;
+        }
+    }
+
+    if (!empty($missingFields)) {
+        echo json_encode([
+            'message' => 'Missing required fields',
+            'missing_fields' => $missingFields
+        ]);
+        exit;
+    }
+    /*
+    //Set ID to update
+    if (!isset($data->id, $data->quote, $data->author_id, $data->category_id)) {
+        echo json_encode(['message' => 'Missing Required Parameters']);
+        exit;
+    }
+    */
+    
+    //Set ID to update
+    //$Quote->id = $data->id;
+    $Quote->quote = $data->quote;
+    $Quote->author_id = $data->author_id;
+    $Quote->category_id = $data->category_id;
+
+    
+
+    //Update quote
+    if($Quote->update()){
+        echo json_encode(
+            array(
+                
+                'id' => $Quote->id,
+                'quote' => $Quote->quote,
+                'author_id' => $Quote->author_id,
+                'category_id' => $Quote->category_id
+                
+            )
+        );
+    } else {
+        echo json_encode(
+            array('message' => "No Quotes Found")
+        );
+    }
