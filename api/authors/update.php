8452cff3510733e54f2e32546ce7e39510dc600d
Kyle Cassity <166655209+Kyntronos@users.noreply.github.com>
2025-03-25 19:05:14 -0500

    reuploading models and api
    made some changes to models and api update

---
+++ api/authors/update.php
@@ -0,0 +1,44 @@
+<?php
+    header('Access-Control-Allow-Origin: *');
+    header('Content-Type: application/json');
+    header('Access-Control-Allow-Methods: PUT');
+    header('Access-Control-Allow-Methods: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Authorization, X-Requested-With');
+
+    include_once('../../config/Database.php');
+    include_once('../../models/Authors.php');
+
+    $database = new Database();
+    $db = $database->connect();
+
+    //Instantiate quote
+
+    $Author = new Authors($db);
+
+    //Get raw data
+    $data = json_decode(file_get_contents('php://input'));
+
+    if (!isset($data->author)) {
+        echo json_encode(['message' => 'Missing Required Parameters']);
+        exit;
+    }
+
+    //Set ID to update
+    $Author->id = $data->id;
+    $Author->author = $data->author;
+
+    
+
+    //Update quote
+    if($Author->update()){
+        echo json_encode(
+            array(
+                'id' => $Author->id,
+                'author'=> $Author->author
+            )
+        );
+    }/* else {
+        echo json_encode(
+            array('message' => "Author Not updated")
+        );
+    }
+    */
